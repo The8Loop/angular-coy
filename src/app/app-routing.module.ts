0c212ba57964482bec2909d79a9a6ab61a267187
E.S. Luper <eric.s.luper@protonmail.com>
2022-02-15 15:41:27 +0100

    FEATURE: Added two page components and routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './pages/create/create.component';
+import { ListComponent } from './pages/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+  { path: 'create', component: CreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
