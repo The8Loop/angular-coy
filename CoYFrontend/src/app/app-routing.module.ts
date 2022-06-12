c72f35d25dcbfe7b91335304d7bb2d8ba70369dd
E.S. Luper <eric.s.luper@protonmail.com>
2022-06-11 19:32:19 -0500

    Feature: Implemented Home Page
    -Added a home page
    -Header routes to home page
    -Added nav button for /list page
    -Financial-listComponent displays negative user total as red.

--- CoYFrontend/src/app/app-routing.module.ts
+++ CoYFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './pages/create/create.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'list', component: ListComponent },
   { path: 'create', component: CreateComponent }
 ];
